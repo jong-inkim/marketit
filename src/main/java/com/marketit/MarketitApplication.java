2cafe267ebda8207a1fe3a5d07a8382c8c05e182
jongin-kim <tlddbs1@naver.com>
2023-08-26 13:00:18 +0900

    first commit

---
+++ src/main/java/com/marketit/MarketitApplication.java
@@ -0,0 +1,13 @@
+package com.marketit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketitApplication.class, args);
+	}
+
+}
